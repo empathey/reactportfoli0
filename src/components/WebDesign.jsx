58040467209a648d37fd3a7ba354a238df01fddf
Christian Szewczul <cszewczul@mail.roosevelt.edu>
2023-09-17 22:30:39 -0500

    pepes revengeq32456789

--- src/components/WebDesign.jsx
+++ src/components/WebDesign.jsx
@@ -2,7 +2,7 @@ import { OrbitControls, Stage } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
 import styled from "styled-components";
-import Mac from "./Mac";
+
 
 const Desc = styled.div`
   width: 200px;
@@ -29,7 +29,7 @@ const WebDesign = () => {
       <Canvas>
         <Suspense fallback={null}>
           <Stage environment="city" intensity={0.6}>
-            <Mac />
+           
           </Stage>
           <OrbitControls enableZoom={false} autoRotate />
         </Suspense>
